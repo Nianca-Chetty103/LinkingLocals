00afbe10708589f2a5a9292ea7c483b3cd712917
Nianca Chetty <niancachetty102@gmail.com>
2026-06-01 18:19:39 +0200

    Add files via upload

---
+++ Approved.php
@@ -0,0 +1,17 @@
+<?php
+include "database.php";
+
+if (isset($_GET['id'])) {
+
+    $id = intval($_GET['id']);
+
+    $stmt = $conn->prepare("UPDATE Users SET status='approved' WHERE id=?");
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        echo "approved";
+    } else {
+        echo "error";
+    }
+}
+?>
